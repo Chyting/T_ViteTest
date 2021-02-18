c9d91565ab80ac2d634d1236a2b23b3003dff5da
胡永婷 <huyt@mti-sh.cn>
2021-02-18 13:57:04 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-<!--    <ul class="li-style">-->
-<!--      <li v-for="(stu,index) in student" :key="stu.id">-->
-<!--        {{stus.id}} -&#45;&#45;&#45;&#45; {{stus.name}}-->
-<!--      </li>-->
-<!--    </ul>-->
-    <div>{{flag}}</div>
   </div>
 </template>
 
 <script>
-import {reactive,toRefs} from 'vue'
 export default {
-  // name: 'App',
-  setUp(){
-    const state = reactive({
-      flag:1
-      // student:[
-      //   {id:0,name:'zs', age:10},
-      //   {id:1,name:'ls', age:30},
-      //   {id:2,name:'ww', age:30}
-      //   ]
-    })
-    return { ...toRefs(state) }
-  }
+  name: 'App',
 }
 </script>
 <style>
