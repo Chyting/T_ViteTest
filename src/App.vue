7c9c7924159b21b017956641df51bb8cb77be9b8
胡永婷 <huyt@mti-sh.cn>
2021-02-18 11:03:58 +0800

    feat:Vite搭建项目

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+<!--    <ul class="li-style">-->
+<!--      <li v-for="(stu,index) in student" :key="stu.id">-->
+<!--        {{stus.id}} -&#45;&#45;&#45;&#45; {{stus.name}}-->
+<!--      </li>-->
+<!--    </ul>-->
+    <div>{{flag}}</div>
+  </div>
+</template>
+
+<script>
+import {reactive,toRefs} from 'vue'
+export default {
+  // name: 'App',
+  setUp(){
+    const state = reactive({
+      flag:1
+      // student:[
+      //   {id:0,name:'zs', age:10},
+      //   {id:1,name:'ls', age:30},
+      //   {id:2,name:'ww', age:30}
+      //   ]
+    })
+    return { ...toRefs(state) }
+  }
+}
+</script>
+<style>
+.li-style{
+  list-style: none;
+  font-size:25px
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item,index)"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      stu:[{
+        name:'zs',
+        age:10
+      },{
+        name:'ls',
+        age:30
+      },
+        {
+          name:'ww',
+          age:30
+        }]
+    }
+  }
+}
+</script>
